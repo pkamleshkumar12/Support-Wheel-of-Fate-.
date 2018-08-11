85c1e4a015cd26e7012e6bc1f226c82ce4ecbe1f
pkamleshkumar12 <pkamleshkumar12@gmail.com>
2018-08-11 14:05:26 +0800

    First commit from SPRING INITIALIZR

---
+++ src/main/java/com/companyx/wheeloffate/WheeloffateApplication.java
@@ -0,0 +1,12 @@
+package com.companyx.wheeloffate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheeloffateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WheeloffateApplication.class, args);
+	}
+}
---
+++ src/test/java/com/companyx/wheeloffate/WheeloffateApplicationTests.java
@@ -0,0 +1,16 @@
+package com.companyx.wheeloffate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WheeloffateApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
